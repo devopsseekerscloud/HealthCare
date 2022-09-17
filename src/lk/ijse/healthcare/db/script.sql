180316180969eafd331a962ef98c7a2117b3edd6
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-09-17 09:24:57 -0700

    4

---
+++ src/lk/ijse/healthcare/db/script.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS `healthcare` ;
+USE `healthcare` ;
+
+CREATE TABLE IF NOT EXISTS `doctor` (
+    `dId` VARCHAR(15) NOT NULL,
+    `name` VARCHAR(45) NULL DEFAULT NULL,
+    `address` TEXT NULL DEFAULT NULL,
+    `contact` VARCHAR(15) NULL DEFAULT NULL,
+    PRIMARY KEY (`dId`));
+
+CREATE TABLE IF NOT EXISTS `patient` (
+    `pId` VARCHAR(15) NOT NULL,
+    `name` VARCHAR(45) NULL DEFAULT NULL,
+    `address` TEXT NULL DEFAULT NULL,
+    `contact` VARCHAR(15) NULL DEFAULT NULL,
+    PRIMARY KEY (`pId`))
