567918df32be14db357fe571d0c5a3b3f440f459
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-09-17 10:57:26 -0700

    11

--- src/lk/ijse/healthcare/controller/DoctorFormController.java
+++ src/lk/ijse/healthcare/controller/DoctorFormController.java
@@ -1,26 +1,48 @@
 package lk.ijse.healthcare.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import lk.ijse.healthcare.dao.DatabaseAccessCode;
 import lk.ijse.healthcare.entity.Doctor;
+import lk.ijse.healthcare.view.tm.DoctorTm;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DoctorFormController {
     public TextField txtName;
     public TextField txtAddress;
     public TextField txtContact;
     public TextField txtId;
-    public TableView tblDoctors;
+    public TableView<DoctorTm> tblDoctors;
     public TableColumn colId;
     public TableColumn colName;
     public TableColumn colAddress;
     public TableColumn colContact;
     public TableColumn colOption;
+
+    public void initialize(){
+        searchData();
+    }
+
+    private void searchData() {
+        try{
+            ArrayList<Doctor> lst = new DatabaseAccessCode().searchDoctors("");
+            ObservableList<DoctorTm> tmList = FXCollections.observableArrayList();
+
+            for (Doctor d:lst
+                 ) {
+                Button btn= new Button("Delete");
+                tmList.add(new DoctorTm(d.getDid(),d.getName(),d.getAddress(),d.getContact(),btn));
+            }
+            tblDoctors.setItems(tmList);
+
+        }catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 
 
     public void saveDoctorOnAction(ActionEvent actionEvent) {
