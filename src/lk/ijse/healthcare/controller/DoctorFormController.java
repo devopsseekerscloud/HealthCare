4786c0c29a66093b8be018ab7bbd00d2fce72c60
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-09-17 09:19:48 -0700

    3

--- src/lk/ijse/healthcare/controller/DoctorFormController.java
+++ src/lk/ijse/healthcare/controller/DoctorFormController.java
@@ -1,4 +1,15 @@
 package lk.ijse.healthcare.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class DoctorFormController {
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+    public TextField txtId;
+
+    public void saveDoctorOnAction(ActionEvent actionEvent) {
+
+    }
 }
